f34ea81ff4f78c1195a6ce194e9f5df73991ab74
Tanesal <dguchinsky@yahoo.com>
2021-09-09 13:52:37 +0600

    refactor: update backend api endpoint

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://njbq98m4dd.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://m6olp3sab1.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
